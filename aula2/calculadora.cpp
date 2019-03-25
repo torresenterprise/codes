e4e9718318830575962c1fee7d6430eeeeb71718
João Victor Torres Borges <borgesjvt@gmail.com>
2019-03-25 10:39:00 -0300

    feat: creating operations

---
+++ aula2/calculadora.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int soma(int a, int b) {
+	return a+b;
+}
+
+int subtracao(int a, int b) {
+	return a-b;
+}
+
+int multiplicacao(int a, int b) {
+	return a*b;
+}
+/*
+int divisao(int a, int b) {
+
+}
+*/
+
+int main() {
+	
+	return 0;
+}
