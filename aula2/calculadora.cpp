c111e723bd653d326b974438489093b1093efc51
João Victor Torres Borges <borgesjvt@gmail.com>
2019-03-25 10:50:18 -0300

    feat: test program

--- aula2/calculadora.cpp
+++ aula2/calculadora.cpp
@@ -19,7 +19,22 @@ int divisao(int a, int b) {
 }
 */
 
+int calculadora(int op, int a, int b) {
+
+	int ret;
+
+	if(op == 0) ret = soma(a,b);
+	else if(op == 1) ret = subtracao(a,b);
+	else if(op == 2) ret = multiplicacao(a,b);
+}
+
 int main() {
-	
+	cout << "Essa é uma calculadora básica. Digite:" << endl << endl;
+	cout << "0 - soma:" << endl;
+	cout << "1 - subtração:" << endl;
+	cout << "2 - multiplicação:" << endl;
+	int op, a, b;
+	cin >> op >> a >> b;
+	calculadora(op, a, b);
 	return 0;
 }
